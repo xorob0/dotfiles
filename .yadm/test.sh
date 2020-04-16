3e05f5508719c3d527751e4b6a8a518452c1cffe
Tim Simon <timothee@hackages.io>
2020-04-16 12:07:05 +0200

    updating test to work with both possibilities

--- .yadm/test.sh
+++ .yadm/test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 ls /usr/share/applications/
-if [ "$(xdg-settings get default-web-browser)" = "org.qutebrowser.qutebrowser.desktop" ];then
+if [ "$(xdg-settings get default-web-browser)" = "org.qutebrowser.qutebrowser.desktop"|| "$(xdg-settings get default-web-browser)" = "qutebrowser.desktop" ];then
 	echo "✅ qutebrowser is the default browser"
 else
 	echo "❌ qutebrowser is not the default browser"
