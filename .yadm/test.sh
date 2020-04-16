2c1544fa64320fbc413959f47391f1bf4f6b256e
Tim Simon <timothee@hackages.io>
2020-04-16 11:14:19 +0200

    Added explaination for default browser test

--- .yadm/test.sh
+++ .yadm/test.sh
@@ -4,6 +4,7 @@ if [ "$(xdg-settings get default-web-browser)" = "org.qutebrowser.qutebrowser.de
 	echo "✅ qutebrowser is the default browser"
 else
 	echo "❌ qutebrowser is not the default browser"
+	echo "The default browser is still $(xdg-settings get default-web-browser)"
 	exit 1
 fi
 
