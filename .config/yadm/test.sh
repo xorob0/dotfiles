2360b2c4cc1233abe84f32de48e77c5855667592
Tim Simon <timothee@hackages.io>
2020-04-16 10:29:27 +0200

    Removed old bootstrap

--- .config/yadm/test.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-if [ "$(xdg-settings get default-web-browser)" = "org.qutebrowser.qutebrowser.desktop" ];then
-	echo "✅ qutebrowser is the default browser"
-else
-	echo "❌ qutebrowser is not the default browser"
-	exit 1
-fi
-
